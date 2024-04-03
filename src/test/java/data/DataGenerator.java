65c71553f1636fd7c18d0be8b50143bb67617ae4
ibuypower <danroz1995@yandex.ru>
2024-04-03 20:48:06 +0300

    Correct

--- src/test/java/data/DataGenerator.java
+++ src/test/java/data/DataGenerator.java
@@ -6,6 +6,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import lombok.Value;
 import lombok.val;
 
 import java.util.Locale;
@@ -63,59 +64,10 @@ public class DataGenerator {
         }
     }
 
-    public static final class RegistrationDto {
-        private final String login;
-        private final String password;
-        private final String status;
-
-        public RegistrationDto(String login, String password, String status) {
-            this.login = login;
-            this.password = password;
-            this.status = status;
-        }
-
-        public String getLogin() {
-            return this.login;
-        }
-
-        public String getPassword() {
-            return this.password;
-        }
-
-        public String getStatus() {
-            return this.status;
-        }
-
-        public boolean equals(final Object o) {
-            if (o == this) return true;
-            if (!(o instanceof RegistrationDto)) return false;
-            final RegistrationDto other = (RegistrationDto) o;
-            final Object this$login = this.getLogin();
-            final Object other$login = other.getLogin();
-            if (!Objects.equals(this$login, other$login)) return false;
-            final Object this$password = this.getPassword();
-            final Object other$password = other.getPassword();
-            if (!Objects.equals(this$password, other$password)) return false;
-            final Object this$status = this.getStatus();
-            final Object other$status = other.getStatus();
-            if (!Objects.equals(this$status, other$status)) return false;
-            return true;
-        }
-
-        public int hashCode() {
-            final int PRIME = 59;
-            int result = 1;
-            final Object $login = this.getLogin();
-            result = result * PRIME + ($login == null ? 43 : $login.hashCode());
-            final Object $password = this.getPassword();
-            result = result * PRIME + ($password == null ? 43 : $password.hashCode());
-            final Object $status = this.getStatus();
-            result = result * PRIME + ($status == null ? 43 : $status.hashCode());
-            return result;
-        }
-
-        public String toString() {
-            return "DataGenerator.RegistrationDto(login=" + this.getLogin() + ", password=" + this.getPassword() + ", status=" + this.getStatus() + ")";
-        }
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
     }
 }
